929917c63157b61f2f321dd00f8b08af562686c2
juliofsilvajr <juliorn3@hotmail.com>
2023-08-11 11:53:19 -0300

    11/08

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,42 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for, request, flash 
+from app.forms import Contato
 
 @app.route('/')
-def home():
-    return render_template('home.html' ,titulo = 'pagina inicial')
+def index():
+    return render_template('index.html',titulo = 'Página inicial')
 
-
-@app.route('/contatos')
+@app.route('/contatos', methods=['POST', 'GET'])
 def contatos():
-    return render_template('contatos.html' , titulo = 'contatos')
+    dados_formulario = None
+    formulario = Contato()
+    print('Acessou a rota contatos!')
+    if formulario.validate_on_submit():
 
+        
+        nome = formulario.nome.data
+        email = formulario.email.data
+        telefone = formulario.telefone.data
+        mensagem = formulario.mensagem.data
+        print('O formulario foi validado!')
+        print(nome)
+        print(email)
+        print(telefone)
+        print(mensagem)
+
+        dados_formulario = {
+            'nome': nome,
+            'email': email,
+            'telefone' : telefone,
+            'mensagem' : mensagem
+        }
+
+    return render_template('contatos.html', titulo = 'Contatos',formulario = formulario,dados_formulario = dados_formulario)
 
 @app.route('/sobre')
 def sobre():
-    return render_template('sobre.html' , titulo = 'sobre')
-
+    return render_template('sobre.html', titulo = 'Sobre')
 
 @app.route('/projetos')
 def projetos():
-    return render_template('projetos.html' , titulo = 'projetos')
+    return render_template('projetos.html', titulo = 'Projetos' )
